2baa6a7ebbaaea06b7777da5bbb5a69e6a09f7b7
huyhoangy <huyhoangwf@gmail.com>
2024-08-23 20:45:54 +0700

    add file html, css, js

---
+++ JS/song.js
@@ -0,0 +1,45 @@
+let songs = [
+    {
+    name: "HỘI PHÁP SƯ FAIRY TAIL REMIX",
+    path: "media/fairry-tail.m4a",
+    artist: "QUANG NHAT REMIX",
+    image: "images/phap.jpg",
+    },
+    {
+      name: "HỒNG NHAN",
+      path: "media/Hồng Nhan - Jack (G5R).mp3",
+      artist: "J97",
+      image: "images/hongnhan.jpg",
+    },
+    {
+      name: "KẸO BÔNG GÒN",
+      path: "media/Kẹo Bông Gòn Orinn EDM Remix - H2K KN.mp3",
+      artist: "H2K",
+      image: "images/keobonggon.jpg",
+    },
+    {
+      name: "XÍCH THÊM CHÚT",
+      path: "media/y2mate.com - Xích Thêm Chút  XTC Remix  RPT Groovie ft TLinh x RPT MCK Prod by fatbenn  RPT LT RAPITALOVE.mp3",
+      artist: "RPT",
+      image: "images/yêu.jpg",
+    },
+    {
+      name: "NHƯ MỘT NGƯỜI DƯNG",
+      path: "media/y2mate.com - Như Một Người Dưng  Nguyễn Thạc Bảo NgọcCukak Remix Audio Lyrics Video.mp3",
+      artist: "CukCak remix",
+      image: "images/1nguoi.jpg",
+    },
+    {
+        name: "AI KHÓC CHO EM ",
+        path: "media/y2mate.com - Ai Khóc Cho Em Remix Dn Team Remix  Huyền Zoe x TVk  Khóc Cho Người Ai Khóc Cho Em.mp3",
+        artist: "Huyển Zoe",
+        image: "images/ai.jpg",
+      },
+      {
+        name: "TÁT NHẬT LÃNG RỰC RỠ ",
+        path: "media/y2mate.com - Tát Nhật Lãng Rực Rỡ Vocal Việt  Fanny Trần  Quaniam Remix  Audio Lyrics Video.mp3",
+        artist: "Fanny Trần",
+        image: "images/tat.jpg ",
+      },
+  ];
+  
